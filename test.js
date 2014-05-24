cf850db9618e513d3cfbffa64d48b7396959958e
James Lynn <jamespdlynn@gmail.com>
2014-05-24 18:38:08 -0500

    Added github web event hook

--- test.js
+++
@@ -1,7 +0,0 @@
-require('child_process').exec("/bin/bash /usr/bin/reset-spacewars.sh", function(error, stdout, stderr){
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-        console.log('exec error: ' + error);
-    }
-});
